8764e98365f4085b9369bd86ebe58165f02eabb0
vishwas-garg <vgarg1809@gmail.com>
2017-04-27 15:47:43 +0530

    Update aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update({
-  credentials: Aws::Credentials.new("Key", "Secret")
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY'], ENV['AWS_ACCESS_SECRET'])
 })
 
-s3 = Aws::S3::Resource.new(region: 'region')
-S3_BUCKET = s3.bucket('bucket_name')
+s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
+S3_BUCKET = s3.bucket(ENV['S3_BUCKET'])
